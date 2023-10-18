93d4504fecf6dbd7a00cf9b9493dee6496e6a424
Jampalavenkatasai <jampalavenkatsai99@gmail.com>
2023-10-18 23:05:16 +0530

    firstcommit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+//here implementing the API that can return the values
+func main() {
+	route := gin.Default()
+	fmt.Println(route)
+
+}
